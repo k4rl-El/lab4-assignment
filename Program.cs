6f024a320e7ae1a900bdf7e4ed588155191eddc8
CHUKWUMA <chukwuma1497@gmail.com>
2022-04-28 22:15:11 +0100

    second commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab4_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  lab4 question: given two numbers, return true if the sum of both numbers is less than 100, otherwise return false
+            Console.WriteLine("input the first number");
+            int first_num = int.Parse(Console.ReadLine());
+            Console.WriteLine("input the second number");
+            int second_num  = int.Parse(Console.ReadLine());
+
+            bool x = first_num + second_num < 100;
+            Console.WriteLine(x);
+        }
+    }
+}
